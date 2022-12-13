46465b46b43cb6ae80daa11ee088d980ace38cbe
joredjs <odraude160@gmail.com>
2022-12-13 18:22:40 -0500

    feat (home): Creacion Componente

---
+++ pages/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+function Header({ title }) {
+  const defaulTitle = "Página de prueba"
+
+  return <h1>{title ? title : defaulTitle}</h1>
+}
+
+export default function HomePage() {
+  const names = ["Página", "Prueba", "de", "asd", "asddasd"]
+  const [likes, setLikes] = useState(0)
+
+  function handleClick() {
+    setLikes(likes + 1)
+  }
+
+  return (
+    <div>
+      <Header title="Starting with next" />
+      <ul>
+        {names.map((x) => (
+          <li key={x}>{x}</li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Like {likes}</button>
+    </div>
+  )
+}
